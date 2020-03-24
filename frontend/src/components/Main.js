6e13529504765aaaad5c15444c037c069cc15e56
rnarain <35756571+rnarain@users.noreply.github.com>
2020-03-24 05:59:41 -0700

    Initial commit
    Add mongodb connection and created login page ui

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Route,Switch} from 'react-router-dom';
+
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+// import StudentHome from './Student/Home/Home';
+
+import Navbar from './CommonComponents/Navbar';
+import { Redirect } from 'react-router';
+
+
+//Create a Main Component
+class Main extends Component {
+    render(){
+        let navRoute = null;
+        if (localStorage.getItem('id')) {
+            navRoute = <Navbar />
+            }
+        else{
+        }
+
+        return(
+            <div>
+                {/*Render Different Component based on Route*/}
+                {navRoute}
+                <Route exact path="/" render={() => <Redirect to="/login" />} />
+                <Route path="/login" component={Login}/>
+                {/* <Route path="/student/profile/:id" component={StudentHome}/> */}
+            </div>
+        )
+    }
+}
+//Export The Main Component
+export default Main;
